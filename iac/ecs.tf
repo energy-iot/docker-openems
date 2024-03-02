e7abc78a5d5aebf99339b223ccf452775adbf89d
Bello Afeez <92779974+werkmanne@users.noreply.github.com>
2024-03-02 16:49:45 +0100

    Update ecs.tf
    Signed-off-by: Bello Afeez <92779974+werkmanne@users.noreply.github.com>
    Signed-off-by: Will Sams <will@samswebs.com>

--- iac/ecs.tf
+++ iac/ecs.tf
@@ -24,7 +24,11 @@ resource "aws_ecs_task_definition" "ecs_task_definition" {
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
   cpu                      = 8192
+<<<<<<< HEAD
   memory                   = 20480
+=======
+  memory                   = 30720
+>>>>>>> 71ffc50 (Update ecs.tf)
 
   runtime_platform {
     operating_system_family = "LINUX"
