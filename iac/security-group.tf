de34f82262da2e36a58332b7f223af2788b07278
belloafeez <werkjober@gmail.com>
2024-03-03 14:58:14 +0100

    updated security group
    Signed-off-by: Will Sams <will@samswebs.com>

--- iac/security-group.tf
+++ iac/security-group.tf
@@ -1,13 +1,21 @@
 # create security group for the application load balancer
 resource "aws_security_group" "openems_security_group" {
   name        = "${var.project_name}-${var.environment}-openems-sg"
-  description = "enable LL access on port 80/443"
+  description = "enable access on port 80/8089"
   vpc_id      = aws_vpc.vpc.id
 
   ingress {
     description = "all traffic"
-    from_port   = 0
-    to_port     = 0
+    from_port   = 8089
+    to_port     = 8089
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "all traffic"
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
