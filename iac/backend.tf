c2754d10e77b980d37a38b566d7ddf8d1f261ea6
belloafeez <werkjober@gmail.com>
2024-05-03 21:11:23 +0100

    Signed-off-by: belloafeez <werkjober@gmail.com>
    fixed iac folder

--- iac/backend.tf
+++ iac/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "openems-deployment-github-action-state-file"
+    bucket         = "openems-deployment-tf-state-file"
     key            = "openems-app/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-state-lock-openems-deployment"
