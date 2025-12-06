2017eec9eddd7e09a6618f4840b459fb3986ff52
Venky <116882464+Venkata-Sai-Vishwanath-robo@users.noreply.github.com>
2025-12-07 00:54:02 +0530

    Revert changes made to backend.tf
    Signed-off-by: Venky <116882464+Venkata-Sai-Vishwanath-robo@users.noreply.github.com>

--- iac/backend.tf
+++ iac/backend.tf
@@ -1,7 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "eiot-openems-tf-state-file"
+    bucket         = "openems-deployment-tf-state-file"
     key            = "openems-app/terraform.tfstate"
     region         = "us-east-1"
+    dynamodb_table = "terraform-state-lock-openems-deployment"
   }
 }
