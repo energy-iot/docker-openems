70fbd54b73363283eaac0cbfc81c47173c541d9f
Venky <116882464+Venkata-Sai-Vishwanath-robo@users.noreply.github.com>
2025-12-07 00:18:51 +0530

    Change S3 bucket name for Terraform backend
    Signed-off-by: Venky <116882464+Venkata-Sai-Vishwanath-robo@users.noreply.github.com>

--- iac/backend.tf
+++ iac/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "openems-deployment-tf-state-file"
+    bucket         = "eiot-openems-tf-state-file"
     key            = "openems-app/terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "terraform-state-lock-openems-deployment"
+    use_lockfile   = "true"
   }
 }
