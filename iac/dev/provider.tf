c492c396e9e0a61958682ea5d34833942cbb2826
Aidan Barnes <66229298+aidan-barnes-axm@users.noreply.github.com>
2026-04-19 23:16:27 -0400

    Update Environment tag from "dev" to "aidev"
    Signed-off-by: Aidan Barnes <66229298+aidan-barnes-axm@users.noreply.github.com>

--- iac/dev/provider.tf
+++ iac/dev/provider.tf
@@ -4,7 +4,7 @@ provider "aws" {
   default_tags {
     tags = {
       Project     = "openems"
-      Environment = "dev"
+      Environment = "aidev"
       ManagedBy   = "terraform"
       Component   = "openems-stack"
     }
